ad3f39d3ede29c1216c095449fc37f1ac7ba137d
Odintsov Timur <timodin@yandex.ru>
2020-08-25 18:31:34 +0300

    api.views: add Category and Genre destroy classes

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics, filters
+from rest_framework import viewsets, generics, filters, mixins
 
 from .models import Category, Genre, Title
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
@@ -7,6 +7,8 @@ from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    # filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('category', 'genre', 'name', 'year')
 
 
 class CategoryList(generics.ListCreateAPIView):
@@ -16,8 +18,21 @@ class CategoryList(generics.ListCreateAPIView):
     search_fields = ('=name',)
 
 
+class CategoryDestroy(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+
 class GenreList(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('=name',)
+
+
+class GenreDestroy(generics.DestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
+    lookup_field = 'slug'

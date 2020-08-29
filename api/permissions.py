d13c331d81c02937dfe124192f9155ea3a0a32b1
tetyorkin <tetyorkin@gmail.com>
2020-08-30 01:30:04 +0600

    Merge anton_tetyorkin

--- api/permissions.py
+++ api/permissions.py
@@ -5,28 +5,3 @@ class AdminPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return bool(request.user.is_staff or request.user.role == 'admin')
-
-
-class IsAccountAdminOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return request.user.is_staff
-
-
-class RoleAdminPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.role == 'admin' or request.user.is_superuser:
-            return True
-        return False
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj.author == request.user

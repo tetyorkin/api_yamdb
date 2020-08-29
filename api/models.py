d496d4c31c3edd711d30508ca425f105c68093aa
tetyorkin <tetyorkin@gmail.com>
2020-08-29 22:00:40 +0600

    edit nodel, new DB

--- api/models.py
+++ api/models.py
@@ -1,5 +1,21 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **kwargs):
+        user = self.model(email=email, is_staff=True, is_superuser=True, **kwargs)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class User(AbstractUser):
@@ -11,7 +27,9 @@ class User(AbstractUser):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=10, choices=UserRole.choices, default=UserRole.user)
-    bio = models.TextField(max_length=500, blank=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True, null=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True, null=True)
 
     def __str__(self):
         return self.username
@@ -30,11 +48,12 @@ class Genre(models.Model):
 class Title(models.Model):
     name = models.CharField(max_length=100)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
+    genre = models.ManyToManyField(Genre)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='titles')
 
 
 class Review(models.Model):
-    title_id = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='review')
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='review')
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='review')
     score = models.IntegerField()
@@ -45,7 +64,7 @@ class Review(models.Model):
 
 
 class Comment(models.Model):
-    review_id = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comment')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comment')
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
     pub_date = models.DateTimeField(auto_now_add=True)

34e3f5f723ec832324a28b8f9f6d3e1e4f2da39f
Odintsov Timur <timodin@yandex.ru>
2020-08-31 12:53:56 +0300

    api.urls: add token urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import views
 
@@ -10,7 +11,9 @@ router.register(r'titles', views.TitleViewSet)
 urlpatterns = [
     path('v1/', include(router.urls)),
     path('v1/genres/', views.GenreList.as_view()),
-    path('v1/genres/<slug:slug>', views.GenreDestroy.as_view()),
+    path('v1/genres/<slug:slug>/', views.GenreDestroy.as_view()),
     path('v1/categories/', views.CategoryList.as_view()),
-    path('v1/categories/<slug:slug>', views.CategoryDestroy.as_view()),
+    path('v1/categories/<slug:slug>/', views.CategoryDestroy.as_view()),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

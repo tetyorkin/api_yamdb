fce02cc0d15cf9bff9f6ee595b9f5546f47147ad
Odintsov Timur <timodin@yandex.ru>
2020-08-31 12:51:45 +0300

    api_yamdb.settings: add rest framework settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -122,3 +122,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+REST_FRAMEWORK = {
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        ],
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ],
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 100
+}
